e260a11d5a7956a2a145228d0462c3fa908ac1a7
Nicholas Dover <dovern42@gmail.com>
2019-02-10 05:01:03 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,6 +8,7 @@ def reformat_languages(languages)
         new_languages[program][:style].push(style1)
       else
         languagelog.push(program)
+        new_languages[program]=elements.merge(style: style1)
       end
     end
   end
