3199d35ada93f678c6d08ddafd18c80f8bfa735a
Nicholas Dover <dovern42@gmail.com>
2019-02-10 05:02:41 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   languagelog=[]
   languages.each do |style1, prog_and_elements|
     prog_and_elements.each do |program, elements|
-      if languagelog.include?(program)
+      if languagelog.include?(program.to_s)
         new_languages[program][:style].push(style1)
       else
         languagelog.push(program.to_s)
