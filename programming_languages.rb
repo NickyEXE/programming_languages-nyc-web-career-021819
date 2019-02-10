48f2e882fabf176080cc7967cdf04b5ed6e77887
Nicholas Dover <dovern42@gmail.com>
2019-02-10 05:02:37 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,13 +1,13 @@
 def reformat_languages(languages)
   # your code here
   new_languages = {}
-  languagelog =[]
+  languagelog=[]
   languages.each do |style1, prog_and_elements|
     prog_and_elements.each do |program, elements|
       if languagelog.include?(program)
         new_languages[program][:style].push(style1)
       else
-        languagelog.push(program)
+        languagelog.push(program.to_s)
         new_languages[program]=elements.merge({style: style1})
       end
     end
